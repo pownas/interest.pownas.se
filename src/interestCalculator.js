20fad170a728fac7fa03347c3d2c2c3f4a0a443d
Jonas Arvidson <pownas@outlook.com>
2022-08-11 22:46:32 +0200

    Create interestCalculator.js
    Added constants

---
+++ src/interestCalculator.js
@@ -0,0 +1,8 @@
+function submitRate() {
+  const inputAmount = document.getElementById('amount');
+  const inputRate = document.getElementById('rate');
+  const inputYears = document.getElementById('years');
+  const outputSummary = document.getElementById('summary');
+  
+  
+}
