b2638f66a1cbb558056ab12ce6ac27119ae40989
Jonas Arvidson <pownas@outlook.com>
2022-08-11 23:17:17 +0200

    Update interestCalculator.js
    Change on variables

--- src/interestCalculator.js
+++ src/interestCalculator.js
@@ -4,11 +4,18 @@ function submitRate() {
   const inputYears = document.getElementById('years')
   const outputSummary = document.getElementById('summary')
   
-  let result = 0;
+  let result = 0
+  let years = 1
+  let rate = 1.00
+  let amount = 0
   
-  for (let i = 0; i < inputYears.value; i++) {
-    result += inputAmount.value
-    result = result * (inputRate.value/100)
+  years = inputYears.value
+  amount = inputAmount.value
+  rate = inputRate.value
+  
+  for (let i = 0; i < years; i++) {
+    result += amount
+    result = result * (rate/100)
   }
   
   outputSummary.innerHTML = result
