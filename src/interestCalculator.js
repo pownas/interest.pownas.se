efb0de6cff10051ef92ce82d1777cf7a739dad70
Jonas Arvidson <pownas@outlook.com>
2022-08-11 23:01:01 +0200

    Update interestCalculator.js

--- src/interestCalculator.js
+++ src/interestCalculator.js
@@ -1,8 +1,13 @@
 function submitRate() {
-  const inputAmount = document.getElementById('amount');
-  const inputRate = document.getElementById('rate');
-  const inputYears = document.getElementById('years');
-  const outputSummary = document.getElementById('summary');
+  const inputAmount = document.getElementById('amount')
+  const inputRate = document.getElementById('rate')
+  const inputYears = document.getElementById('years')
+  const outputSummary = document.getElementById('summary')
   
+  let result = 0;
   
+  for (let i = 0; i < inputYears.value; i++) {
+    result += inputAmount.value
+    result = result * (inputRate.value/100)
+  }
 }
