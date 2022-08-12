e431a5da52822136ca557ca777251dbc7ca21e26
Jonas Arvidson <pownas@outlook.com>
2022-08-12 23:38:47 +0200

    Update interestCalculator.js

--- src/interestCalculator.js
+++ src/interestCalculator.js
@@ -2,7 +2,9 @@ function submitRate() {
   const inputAmount = document.getElementById('amount')
   const inputRate = document.getElementById('rate')
   const inputYears = document.getElementById('years')
-  const inputHowOften = document.getElementByName('howOften')
+  const inputRadioOnce = document.getElementById('once')
+  const inputRadioMonthly = document.getElementById('monthly')
+  const inputRadioYearly = document.getElementById('yearly')
   const outputSummary = document.getElementById('summary')
   
   let result = 0
@@ -15,9 +17,9 @@ function submitRate() {
   rate = parseFloat(inputRate.value)
   
   for (let i = 0; i < years; i++) {
-    if(inputHowOften.value == "yearly" || inputHowOften.value == "once" && i < 1)
+    if(inputRadioYearly.checked || inputRadioOnce.checked && i < 1)
       result = result + amount
-    if(inputHowOften.value == "monthly")
+    if(inputRadioMonthly.checked)
       result = result + (amount*12)
     result = (result * (rate/100)) + result
   }
